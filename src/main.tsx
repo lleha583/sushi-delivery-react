03890e612a17543e189de9dba8cbd33b7d36a7d3
lleha583 <vip.alekseyklepca@mail.ru>
2024-09-05 16:36:38 +0300

    seting

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { route } from './route/route.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={route} />
   </StrictMode>,
 )
